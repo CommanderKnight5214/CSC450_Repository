ada7d2acabaa3ade1f4090ad7d3b0103c7b9dc6b
CommanderKnight5214 <CommanderKnight5214@users.noreply.github.com>
2022-03-31 09:08:51 -0500

    HW8 Commit

---
+++ HW8/Client/chatClient.java
@@ -0,0 +1,22 @@
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class chatClient 
+{
+    public static void main(String[] args) throws Exception
+    {
+        Socket s = new Socket("localhost", 2222); 
+        Scanner clientInput = new Scanner(s.getInputStream());
+        String question = clientInput.nextLine();
+        System.out.println(question);
+        Scanner localInput = new Scanner(System.in);
+        PrintStream clientOutput = new PrintStream(s.getOutputStream());
+
+        //The part below is what I implemented, in a sence I the client to communicate to the server at the very least.
+        clientOutput.println(localInput.nextLine());
+        clientInput = new Scanner(s.getInputStream());
+        question = clientInput.nextLine();
+        System.out.println(question);
+    }
+}
